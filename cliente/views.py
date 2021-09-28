e8a91979f061403610dfa948971a0d1ef323991d
Gyovane <gyovanesouzza@outlook.com.br>
2021-09-27 21:35:24 -0300

    Finish cadastro

--- cliente/views.py
+++ cliente/views.py
@@ -8,24 +8,6 @@ from .forms import ClienteForm, UserForm
 from .models import Cliente
 from django.contrib.auth.models import User
 
-
-class BasePerfil(View):
-    template_name = 'cliente/criar.html'
-
-    def setup(self, *args, **kwargs):
-        super().setup(*args, **kwargs)
-
-        self.context = {
-            'userform': forms.UserForm(data=self.request.POST or None),
-            'clientForm': forms.ClientForm(data=self.request.POST or None),
-
-        }
-        self.renderizar = render(self.request, self.template_name, self.context)
-
-    def get(self, *args, **kwargs):
-        return self.renderizar
-
-
 class Atualizar(View):
     pass
 
@@ -42,13 +24,10 @@ def criar(request):
         formCliente= ClienteForm(request.POST)
 
         usuario = Cliente.popular_usuario(formUser.usuario)
-
-
         cliente = Cliente.popular_cliente(formCliente.cliente,usuario)
 
         usuario.save()
         cliente.save()
-
         return redirect('../produto')
 
 
