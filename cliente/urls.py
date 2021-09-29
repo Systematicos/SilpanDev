2fdda6ac22ace546755ae5bcc80f2d2ecd8bad46
Gyovane <gyovanesouzza@outlook.com.br>
2021-09-28 23:55:00 -0300

    efetuando login

--- cliente/urls.py
+++ cliente/urls.py
@@ -6,7 +6,7 @@ app_name = 'cliente'
 urlpatterns = [
     path("", views.criar, name='criar'),
     path("atualizar", views.Atualizar.as_view(), name='atualizar'),
-    path("login", views.Login.as_view(), name='login'),
+    path("login", views.Login, name='login'),
     path("logout", views.Logout.as_view(), name='logout'),
 
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
@@ -8,30 +9,38 @@ from .forms import ClienteForm, UserForm
 from .models import Cliente
 from django.contrib.auth.models import User
 
-class Atualizar(View):
-    pass
 
-class Login(View):
+class Atualizar(View):
     pass
 
 
 class Logout(View):
     pass
 
+
 def criar(request):
     if request.method == 'POST':
         formUser = UserForm(request.POST)
-        formCliente= ClienteForm(request.POST)
+        formCliente = ClienteForm(request.POST)
 
         usuario = Cliente.popular_usuario(formUser.usuario)
-        cliente = Cliente.popular_cliente(formCliente.cliente,usuario)
+        cliente = Cliente.popular_cliente(formCliente.cliente, usuario)
 
         usuario.save()
         cliente.save()
         return redirect('../produto')
 
 
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        password = request.POST.get('password')
 
+        if not username or not password:
+            return HttpResponse('digtar usuario ou senha')
 
-def popular_endereco(form):
-    pass
+        usuario = authenticate(request, username=username, password=password)
+
+        if not usuario:
+            return HttpResponse('nao tem usuario')
+        return redirect('produtos:lista')
