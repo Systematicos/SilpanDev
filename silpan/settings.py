c4ab095d53cbb896bef24a21342b766c5364b8db
Gyovane <gyovanesouzza@outlook.com.br>
2021-11-13 16:53:31 -0300

    statics

--- silpan/settings.py
+++ silpan/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/templates/static/'
+STATIC_ROOT = BASE_DIR / 'templates/static'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
