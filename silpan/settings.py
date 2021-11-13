0e3a77b6b090b0fd86206502e998051967805dfb
Gyovane <gyovanesouzza@outlook.com.br>
2021-11-13 18:43:16 -0300

    settings db

--- silpan/settings.py
+++ silpan/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'silpan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
