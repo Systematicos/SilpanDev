f9c22abd15171847f4567355cfbdaa6d16afed4d
Gyovane <gyovanesouzza@outlook.com.br>
2021-11-13 18:55:34 -0300

    settings db

--- silpan/settings.py
+++ silpan/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -90,15 +88,12 @@ WSGI_APPLICATION = 'silpan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASE_URL = os.getenv("JAWSDB_MARIA_URL")
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': db_from_env
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
