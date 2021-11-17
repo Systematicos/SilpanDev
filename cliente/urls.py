0e72dfdaf4c1365183faf18d351a00b9d91ff0bd
Deivison <deivison.souza@aluno.faculdadeimpacta.com.br>
2021-11-16 23:11:34 -0300

    Mudanças na tela de detalhamento e recadastramento das imagens no servidor local.

--- cliente/urls.py
+++ cliente/urls.py
@@ -5,7 +5,6 @@ app_name = 'cliente'
 
 urlpatterns = [
     path("", views.criar, name='criar'),
-    path("atualizar", views.Atualizar.as_view(), name='atualizar'),
     path("logout", views.Logout, name='logout'),
     path("login", views.Login, name='login'),
     path("buscar", views.buscarByCPF, name='buscarByCpf'),
